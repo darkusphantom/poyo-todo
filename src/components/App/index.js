e9f4949af7db54761ac28b94e989a7923dd158ae
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-01-18 17:45:04 -0400

    Composicion de estado

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,52 @@ import React from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
+/*
+function App() {
+  const [state, setState] = React.useState('Estado compartido');
+
+  return (
+      <React.Fragment>
+        <TodoHeader >
+          <TodoCounter />
+          <TodoSearch />
+        </TodoHeader>
+
+        <TodoList>
+          <TodoItem state={state} />
+        </TodoList>
+      </React.Fragment>
+  );
+}
+
+function TodoHeader({ children }) {
+  return(
+    <header>
+      {children}
+    </header>
+  );
+}
+
+function TodoList({ children }) {
+  return(
+    <section className="TodoList-container">
+      {children}
+    </section>
+  );
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+
+function TodoItem({ state }) {
+  return <p>TodoItem: {state}</p>;
+}
+*/
 
 function App() {
   return (
@@ -10,5 +56,6 @@ function App() {
     </TodoProvider>
   );
 }
+
 
 export default App;
