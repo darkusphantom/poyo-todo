9d6fb69949180158a2510ff2ac74df250f1cab25
darkusphantom <luisfrodriguezb.lf@gmail.com>
2024-06-09 22:26:58 -0400

    feat: integration vite

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/index';
+
+// const App = () => <h1>Hello World</h1>
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
