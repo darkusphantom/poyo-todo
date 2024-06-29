497fc1f4c60505ddb341adefd3b4b4239fd14742
darkusphantom <luisfrodriguezb.lf@gmail.com>
2024-06-28 23:45:54 -0400

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,12 +19,10 @@ export default defineConfig({
   // },
   server: {
     proxy: {
-      "/api": {
-        target: "https://api.notion.com",
-        secure: false,
+      '/api': {
+        target: 'https://api.notion.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
